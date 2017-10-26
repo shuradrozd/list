4ce4c22cbc9d70bc6102193f373f8f4311525cea
AlexDrozd <shuradrozd@mail.ru>
2017-10-26 17:46:36 +0300

    add editHandler

---
+++ js/editHandler.js
@@ -0,0 +1,20 @@
+var mssql = require('mssql');
+var path = require('path');
+var queries = require('./queries');
+
+module.exports = {
+
+   displayEditItems : function (req, res) {
+        queries.getAllEditItems(req, res);
+   },
+
+    displayOneItem : function (req, res) {
+        queries.getOneEditItem(req, res);
+   },
+
+   editRow: function(req, res) {
+        console.log(req.body);
+        queries.editItem(req.body, req, res);
+    }
+
+};
